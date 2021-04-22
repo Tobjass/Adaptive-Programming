9214a46e93ef2f576b5a8cf775cff999e67dd58b
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-22 22:59:48 +0200

    Add files via upload

---
+++ Formatieve opdrachten/Docenten en Studenten/Docenten.java
@@ -0,0 +1,17 @@
+package com.tobias.ds;
+
+public class Docenten {
+
+    private String naam;
+    private String lijstMetStudenten;
+    private String docenten;
+
+    public Docenten() {
+    }
+
+    public Docenten(String naam, String lijstMetStudenten, String docenten) {
+        this.naam = naam;
+        this.lijstMetStudenten = lijstMetStudenten;
+        this.docenten = docenten;
+    }
+}
