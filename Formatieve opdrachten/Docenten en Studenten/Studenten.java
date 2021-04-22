cb4f48e89f7194713ec8ca1cc18d517c2e980eab
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-22 23:00:06 +0200

    Add files via upload

---
+++ Formatieve opdrachten/Docenten en Studenten/Studenten.java
@@ -0,0 +1,19 @@
+package com.tobias.ds;
+
+public class Studenten {
+
+    private String naam;
+    private int leeftijd;
+    private String lijstMetCijfers;
+    private String huisdier;
+
+    public Studenten() {
+    }
+
+    public Studenten(String naam, int leeftijd, String lijstMetCijfers, String huisdier) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.lijstMetCijfers = lijstMetCijfers;
+        this.huisdier = huisdier;
+    }
+}
