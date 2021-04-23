748ab9e311cd6ce60f0d49338b9403f897e7ef6b
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 13:30:30 +0200

    Update Park.java
    Variables naam, plaats, provincie en beoordeling toegevoegd
    Mogelijkheid om nieuw park in main aan te roepen toegevoegd
    Constructor toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/Park.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Park.java
@@ -1,4 +1,19 @@
 package com.tobias.summatieveopdracht1;
 
 public class Park {
+
+    private String naam;
+    private String plaats;
+    private String provincie;
+    private float beoordeling;
+
+    public Park() {
+    }
+
+    public Park(String naam, String plaats, String provincie, float beoordeling) {
+        this.naam = naam;
+        this.plaats = plaats;
+        this.provincie = provincie;
+        this.beoordeling = beoordeling;
+    }
 }
