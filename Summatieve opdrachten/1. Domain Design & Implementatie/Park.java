18a3c2cc0a09eb523f557a9d97fa753e4b657147
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 14:56:55 +0200

    Update Park.java
    Faciliteiten lijst toegevoegd
    setFaciliteit() toegevoegd
    toString() aangepast

--- Summatieve opdrachten/1. Domain Design & Implementatie/Park.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Park.java
@@ -1,11 +1,15 @@
 package com.tobias.summatieveopdracht1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Park {
 
     private String naam;
     private String plaats;
     private String provincie;
     private double beoordeling;
+    private List<Faciliteit> faciliteiten = new ArrayList<>();
 
     public Park() {
     }
@@ -17,8 +21,12 @@ public class Park {
         this.beoordeling = beoordeling;
     }
 
+    public void setFaciliteit(Faciliteit faciliteit) {
+        this.faciliteiten.add(faciliteit);
+    }
+
     @Override
     public String toString() {
-        return naam + " (" + beoordeling + ")\n   " + plaats + ", " + provincie;
+        return naam + " (" + beoordeling + ")\n   " + plaats + ", " + provincie + "\n\nFaciliteiten: " + faciliteiten;
     }
 }
