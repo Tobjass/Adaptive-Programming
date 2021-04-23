2215e73535f4bb0bd101aa9604989456e0eb4466
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 17:42:34 +0200

    Update Main.java
    3 prints om te laten zien welke klant welk huis heeft gehuurd toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
@@ -14,20 +14,23 @@ public class Main {
         Faciliteit f3 = new Faciliteit("Fietsverhuur", "Bikes & More");
         park.setFaciliteit(f3);
 
-        Huis h1 = new Huis("8-persoons bungalow 8L", 236.33, 122, 4, 2, true, true, true, true, 5, 2, true, true, false);
+        Huis h1 = new Huis("8-persoons bungalow 8L", 235, 122, 4, 2, true, true, true, true, 5, 2, true, true, false);
         park.setHuis(h1);
-        Huis h2 = new Huis("10-persoons bungalow 10L", 319.67, 148, 5, 2, true, true, true, true, 5, 3, true, true, false);
+        Huis h2 = new Huis("10-persoons bungalow 10L", 320, 148, 5, 2, true, true, true, true, 5, 3, true, true, false);
         park.setHuis(h2);
-        Huis h3 = new Huis("16-persoons bungalow 16C", 247.25, 220, 8, 2, true, true, true, true, 8, 4, true, false, false);
+        Huis h3 = new Huis("16-persoons bungalow 16C", 250, 220, 8, 2, true, true, true, true, 8, 4, true, false, false);
         park.setHuis(h3);
 
-        Klant k1 = new Klant("Tobias Kramer", "Achtersloot", 87, "5601 ES", "IJsselstein", "25/06/2002", "0678931803", "tobias35@gmail.com", "M", 10.0);
+        Klant k1 = new Klant("Tobias Kramer", "Achtersloot", 87, "5601 ES", "IJsselstein", "25/06/2002", "0678931803", "tobias35@gmail.com", "M", 10);
         h2.setHuurder(k1);
-        Klant k2 = new Klant("Jeroen Visser", "Bombardonlaan", 56, "2783 PT", "Nieuwegein", "14/03/2002", "0632619375", "jeroen89@gmail.com", "M", 5.0);
+        Klant k2 = new Klant("Jeroen Visser", "Bombardonlaan", 56, "2783 PT", "Nieuwegein", "14/03/2002", "0632619375", "jeroen89@gmail.com", "M", 15);
         h1.setHuurder(k2);
-        Klant k3 = new Klant("Bart van Zandwijk", "Nolensstraat", 9, "9652 AN", "Nieuwegein", "09/11/2002", "0684617593", "bartvz@gmail.com", "M", 15);
+        Klant k3 = new Klant("Bart van Zandwijk", "Nolensstraat", 9, "9652 AN", "Nieuwegein", "09/11/2002", "0684617593", "bartvz@gmail.com", "M", 5);
         h3.setHuurder(k3);
 
         System.out.println(land);
+        System.out.println(h1.getHuurder());
+        System.out.println(h2.getHuurder());
+        System.out.println(h3.getHuurder());
     }
 }
