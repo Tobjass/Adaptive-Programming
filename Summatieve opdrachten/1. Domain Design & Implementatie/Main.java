bb05b77dc8f63a5a3cab9f4c4ba02129992013fa
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 14:27:02 +0200

    Update Main.java
    Nederland aanmaken en printen

--- Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
@@ -1,8 +1,9 @@
 package com.tobias.summatieveopdracht1;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+        Land l1 = new Land("Nederland");
+        System.out.println(l1 + ":");
     }
 }
