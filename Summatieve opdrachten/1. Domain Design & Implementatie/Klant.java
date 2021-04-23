191ef480dcb500628ae9f6ebc78d9f60f5c8c3aa
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 14:20:49 +0200

    Update Klant.java
    Variables naam, straat, huisnummer, postcode, woonplaats, geboortedatum, telefoonnummer, emailadres, manOfVrouw en kortingsPercentage toegevoegd
    Mogelijkheid om nieuwe klant in main aan te roepen toegevoegd
    Constructor toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/Klant.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Klant.java
@@ -1,4 +1,31 @@
 package com.tobias.summatieveopdracht1;
 
 public class Klant {
+
+    private String naam;
+    private String straat;
+    private int huisnummer;
+    private String postcode;
+    private String woonplaats;
+    private String geboortedatum;
+    private int telefoonnummer;
+    private String emailadres;
+    private String manOfVrouw;
+    private double kortingsPercentage;
+
+    public Klant() {
+    }
+
+    public Klant(String naam, String straat, int huisnummer, String postcode, String woonplaats, String geboortedatum, int telefoonnummer, String emailadres, String manOfVrouw, double kortingsPercentage) {
+        this.naam = naam;
+        this.straat = straat;
+        this.huisnummer = huisnummer;
+        this.postcode = postcode;
+        this.woonplaats = woonplaats;
+        this.geboortedatum = geboortedatum;
+        this.telefoonnummer = telefoonnummer;
+        this.emailadres = emailadres;
+        this.manOfVrouw = manOfVrouw;
+        this.kortingsPercentage = kortingsPercentage;
+    }
 }
