ec094448d7c3be1ba827a44c2eff3d07ec3ed96f
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 17:47:22 +0200

    Update Faciliteit.java
    Lege constructor weggehaald

--- Summatieve opdrachten/1. Domain Design & Implementatie/Faciliteit.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Faciliteit.java
@@ -5,9 +5,6 @@ public class Faciliteit {
     private String type;
     private String naam;
 
-    public Faciliteit() {
-    }
-
     public Faciliteit(String type, String naam) {
         this.type = type;
         this.naam = naam;
