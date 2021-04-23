566deeca89abf058b8ac36fc5abee85573a61abd
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 13:25:46 +0200

    Update Land.java
    Variable naam aangemaakt
    Mogelijkheid om nieuw land in main aan te roepen toegevoegd
    Constructor toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/Land.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Land.java
@@ -1,4 +1,13 @@
 package com.tobias.summatieveopdracht1;
 
 public class Land {
+
+    private String naam;
+
+    public Land() {
+    }
+
+    public Land(String naam) {
+        this.naam = naam;
+    }
 }
