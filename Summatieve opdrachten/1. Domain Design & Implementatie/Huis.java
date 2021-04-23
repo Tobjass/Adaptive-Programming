135a809a2f1b9307f195c89a858908c11de87e26
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 13:38:17 +0200

    Update Huis.java
    Variables type, prijsPerDag, oppervlakte, aantalSlaapkamers, aantalVerdiepingen, verwarming, wifi, vaatwasser, koffiemachine, aantalGaspitten, aantalBadkamers, föhn, sauna en zonnebank toegevoegd
    Mogelijkheid om nieuw huis in main aan te roepen toegevoegd
    Constructor toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/Huis.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Huis.java
@@ -1,4 +1,39 @@
 package com.tobias.summatieveopdracht1;
 
 public class Huis {
+
+    private String type;
+    private double prijsPerDag;
+    private int oppervlakte;
+    private int aantalSlaapkamers;
+    private int aantalVerdiepingen;
+    private boolean verwarming;
+    private boolean wifi;
+    private boolean vaatwasser;
+    private boolean koffiemachine;
+    private int aantalGaspitten;
+    private int aantalBadkamers;
+    private boolean föhn;
+    private boolean sauna;
+    private boolean zonnebank;
+    
+    public Huis() {
+    }
+
+    public Huis(String type, double prijsPerDag, int oppervlakte, int aantalSlaapkamers, int aantalVerdiepingen, boolean verwarming, boolean wifi, boolean vaatwasser, boolean koffiemachine, int aantalGaspitten, int aantalBadkamers, boolean föhn, boolean sauna, boolean zonnebank) {
+        this.type = type;
+        this.prijsPerDag = prijsPerDag;
+        this.oppervlakte = oppervlakte;
+        this.aantalSlaapkamers = aantalSlaapkamers;
+        this.aantalVerdiepingen = aantalVerdiepingen;
+        this.verwarming = verwarming;
+        this.wifi = wifi;
+        this.vaatwasser = vaatwasser;
+        this.koffiemachine = koffiemachine;
+        this.aantalGaspitten = aantalGaspitten;
+        this.aantalBadkamers = aantalBadkamers;
+        this.föhn = föhn;
+        this.sauna = sauna;
+        this.zonnebank = zonnebank;
+    }
 }
