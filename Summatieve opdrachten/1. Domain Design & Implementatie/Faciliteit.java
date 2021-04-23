53c28cf55ceb22035428ca4567f4c043b74c8cb0
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-23 13:34:05 +0200

    Update Faciliteit.java
    Variables type en naam toegevoegd
    Mogelijkheid om nieuwe faciliteit in main aan te roepen toegevoegd
    Constructor toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/Faciliteit.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Faciliteit.java
@@ -1,4 +1,15 @@
 package com.tobias.summatieveopdracht1;
 
 public class Faciliteit {
+
+    private String type;
+    private String naam;
+
+    public Faciliteit() {
+    }
+
+    public Faciliteit(String type, String naam) {
+        this.type = type;
+        this.naam = naam;
+    }
 }
