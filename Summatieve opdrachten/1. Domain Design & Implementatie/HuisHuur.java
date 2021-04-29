58fbdc0bf50c00445a33eb5097447fffaae7d311
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-29 15:28:48 +0200

    Add files via upload

---
+++ Summatieve opdrachten/1. Domain Design & Implementatie/HuisHuur.java
@@ -0,0 +1,11 @@
+package tobias.summatieveopdracht1;
+
+public class HuisHuur {
+
+    private int aantalDagen;
+    private Klant huurder;
+    private Huis gehuurdeHuis;
+
+    public HuisHuur() {
+    }
+}
