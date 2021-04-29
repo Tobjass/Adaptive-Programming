53add748876bc8f8c56c25cf4028a57364f5cf38
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-29 15:42:27 +0200

    Update HuisHuur.java
    toString() aangepast
    berekenPrijs() toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/HuisHuur.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/HuisHuur.java
@@ -1,5 +1,7 @@
 package tobias.summatieveopdracht1;
 
+import java.util.Objects;
+
 public class HuisHuur {
 
     private int aantalDagen;
@@ -23,5 +25,19 @@ public class HuisHuur {
 
     public String getGehuurdeHuis() {
         return gehuurdeHuis.getType();
+    }
+
+    private double berekenPrijs() {
+        if (gehuurdeHuis != null && huurder != null) {
+            return aantalDagen * gehuurdeHuis.getPrijsPerNacht() * (1 - huurder.getKorting() / 100);
+        }
+        return 0;
+    }
+    
+    @Override
+    public String toString() {
+        return Objects.requireNonNullElse(gehuurdeHuis.getType(), "\n\ter is geen huis bekend") + "\n\t"
+                + Objects.requireNonNullElse(huurder, "er is geen huurder bekend") + "\n\taantal dagen: "
+                + aantalDagen + " en dat kost " + berekenPrijs();
     }
 }
