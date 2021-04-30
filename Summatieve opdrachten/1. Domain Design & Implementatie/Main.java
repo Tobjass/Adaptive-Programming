958e830292a1c8ca67d028aa7315e4540dd895ea
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-30 17:55:08 +0200

    Update Main.java
    Setters voor subclasses aangepast

--- Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
@@ -27,9 +27,9 @@ public class Main {
         hh1.setGehuurdeHuis(h2);
         Klant k1 = new Klant("Ella Cox", "Middengolf", 23, "4189 IQ", "IJsselstein", "28/09/2003", "0682950192", "ellac@gmail.com", 5);
         Vrouw vr = new Vrouw();
-        vr.setVrouw(k1);
+        vr.setVariable(k1);
         Adolescent a1 = new Adolescent();
-        a1.setAdolescent(k1);
+        a1.setVariable(k1);
 
         HuisHuur hh2 = new HuisHuur();
         Klant k2 = new Klant("Tobias Kramer", "Achtersloot", 87, "5601 ES", "IJsselstein", "25/06/2002", "0678931803", "tobias35@gmail.com", 10);
@@ -37,9 +37,9 @@ public class Main {
         hh2.setAantalNachten(4);
         hh2.setGehuurdeHuis(h2);
         Man m1 = new Man();
-        m1.setMan(k2);
+        m1.setVariable(k2);
         Adolescent a2 = new Adolescent();
-        a2.setAdolescent(k2);
+        a2.setVariable(k2);
 
         Klant k3 = new Klant("Jeroen Visser", "Bombardonlaan", 56, "2783 PT", "Nieuwegein", "14/03/1998", "0632619375", "jeroen89@gmail.com", 15);
         HuisHuur hh3 = new HuisHuur();
@@ -47,9 +47,9 @@ public class Main {
         hh3.setAantalNachten(3);
         hh3.setGehuurdeHuis(h3);
         Man m2 = new Man();
-        m2.setMan(k3);
+        m2.setVariable(k3);
         Volwassene vo = new Volwassene();
-        vo.setVolwassene(k3);
+        vo.setVariable(k3);
 
         Klant k4 = new Klant("Bart van Zandwijk", "Nolensstraat", 9, "9652 AN", "Nieuwegein", "09/11/1955", "0684617593", "bartvz@gmail.com", 5);
         HuisHuur hh4 = new HuisHuur();
@@ -57,9 +57,9 @@ public class Main {
         hh4.setAantalNachten(5);
         hh4.setGehuurdeHuis(h4);
         Man m3 = new Man();
-        m3.setMan(k4);
+        m3.setVariable(k4);
         Senior s = new Senior();
-        s.setSenior(k4);
+        s.setVariable(k4);
 
         System.out.println(land + "\n\nEerste huishuur:\n   " + hh1 + "\n\nTweede huishuur:\n   " + hh2
                 + "\n\nDerde huishuur:\n   " + hh3 + "\n\nVierde huishuur:\n   " + hh4 + "\n\n" + vr + "\n" + a1 + "\n\n"
