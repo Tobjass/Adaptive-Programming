12c282a7622758ae60fe0d646e4b960a82b6e56b
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-30 14:45:09 +0200

    Update Main.java
    Geboortedatums aangepast
    Geslacht en leeftijdsgroep toegevoegd
    toString() aangepast

--- Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/Main.java
@@ -26,35 +26,43 @@ public class Main {
         HuisHuur hh1 = new HuisHuur();
         hh1.setGehuurdeHuis(h2);
         Klant k1 = new Klant("Ella Cox", "Middengolf", 23, "4189 IQ", "IJsselstein", "28/09/2003", "0682950192", "ellac@gmail.com", 5);
-        Vrouw vrouw = new Vrouw();
-        vrouw.setVrouw(k1);
+        Vrouw vr = new Vrouw();
+        vr.setVrouw(k1);
+        Adolescent a1 = new Adolescent();
+        a1.setAdolescent(k1);
 
         HuisHuur hh2 = new HuisHuur();
         Klant k2 = new Klant("Tobias Kramer", "Achtersloot", 87, "5601 ES", "IJsselstein", "25/06/2002", "0678931803", "tobias35@gmail.com", 10);
         hh2.setHuurder(k2);
         hh2.setAantalNachten(4);
         hh2.setGehuurdeHuis(h2);
-        Man man1 = new Man();
-        man1.setMan(k2);
+        Man m1 = new Man();
+        m1.setMan(k2);
+        Adolescent a2 = new Adolescent();
+        a2.setAdolescent(k2);
 
-        Klant k3 = new Klant("Jeroen Visser", "Bombardonlaan", 56, "2783 PT", "Nieuwegein", "14/03/2002", "0632619375", "jeroen89@gmail.com", 15);
+        Klant k3 = new Klant("Jeroen Visser", "Bombardonlaan", 56, "2783 PT", "Nieuwegein", "14/03/1998", "0632619375", "jeroen89@gmail.com", 15);
         HuisHuur hh3 = new HuisHuur();
         hh3.setHuurder(k3);
         hh3.setAantalNachten(3);
         hh3.setGehuurdeHuis(h3);
-        Man man2 = new Man();
-        man2.setMan(k3);
+        Man m2 = new Man();
+        m2.setMan(k3);
+        Volwassene vo = new Volwassene();
+        vo.setVolwassene(k3);
 
-        Klant k4 = new Klant("Bart van Zandwijk", "Nolensstraat", 9, "9652 AN", "Nieuwegein", "09/11/2002", "0684617593", "bartvz@gmail.com", 5);
+        Klant k4 = new Klant("Bart van Zandwijk", "Nolensstraat", 9, "9652 AN", "Nieuwegein", "09/11/1955", "0684617593", "bartvz@gmail.com", 5);
         HuisHuur hh4 = new HuisHuur();
         hh4.setHuurder(k4);
         hh4.setAantalNachten(5);
         hh4.setGehuurdeHuis(h4);
-        Man man3 = new Man();
-        man3.setMan(k4);
+        Man m3 = new Man();
+        m3.setMan(k4);
+        Senior s = new Senior();
+        s.setSenior(k4);
 
         System.out.println(land + "\n\nEerste huishuur:\n   " + hh1 + "\n\nTweede huishuur:\n   " + hh2
-                + "\n\nDerde huishuur:\n   " + hh3 + "\n\nVierde huishuur:\n   +" + hh4 + "\n\n" + vrouw + "\n" + man1
-                + "\n" + man2 + "\n" + man3);
+                + "\n\nDerde huishuur:\n   " + hh3 + "\n\nVierde huishuur:\n   " + hh4 + "\n\n" + vr + "\n" + a1 + "\n\n"
+                + m1 + "\n" + a2 + "\n\n" + m2 + "\n" + vo + "\n\n" + m3 + "\n" + s);
     }
 }
