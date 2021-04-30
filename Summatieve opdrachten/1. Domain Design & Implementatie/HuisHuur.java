321d3e4c4b626db36708ba5990bc106c588838ce
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-04-30 18:03:26 +0200

    Update HuisHuur.java
    Commentaar voor berekenPrijs(), huisCheck() en klantCheck() toegevoegd

--- Summatieve opdrachten/1. Domain Design & Implementatie/HuisHuur.java
+++ Summatieve opdrachten/1. Domain Design & Implementatie/HuisHuur.java
@@ -21,26 +21,33 @@ public class HuisHuur {
         this.gehuurdeHuis = gehuurdeHuis;
     }
 
-    private double berekenPrijs() {
+    public double berekenPrijs() {
         if (gehuurdeHuis != null && huurder != null) {
             return aantalNachten * gehuurdeHuis.getPrijsPerNacht() * (1 - huurder.getKorting() / 100);
         }
         return 0;
     }
+    // Berekent de prijs voor het huis. Als er een huis en huurder is wordt het aantal nachten vermenigvuldigd met de
+    // prijs per nacht van dat huis, en uiteindelijk met de korting van die klant. Als er geen huis of huurder bekend
+    // is de prijs 0.
 
-    private String huisCheck() {
+    public String huisCheck() {
        if (gehuurdeHuis != null) {
            return gehuurdeHuis.getType() + " met prijs per nacht: " + gehuurdeHuis.getPrijsPerNacht();
        }
        return "er is geen huis bekend";
     }
+    // Controleert of er een huis bekend is. Zo ja, wordt het type van dit huis samen met de prijs per nacht als string
+    // returned. Als dit niet het geval is wordt er een string returned.
 
-    private String klantCheck() {
+    public String klantCheck() {
         if (huurder != null) {
             return "op naam van: " + huurder.getNaam() + " (korting: " + huurder.getKorting() + "%)";
         }
         return "er is geen huurder bekend";
     }
+    // Controleert of er een huurder bekend is. Zo ja, wordt de naam van de huurder samen met het kortingspercentage als
+    // string returned. Als dit niet het geval is wordt er een string returned.
 
     @Override
     public String toString() {
