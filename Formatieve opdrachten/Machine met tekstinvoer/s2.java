d823e4c356a1d6f6e05da736436dd9b3e7f4e583
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 23:56:33 +0200

    Update s2.java
    Prints voor opties weggehaald
    Loop weggehaald
    update() returnt nu een string

--- Formatieve opdrachten/Machine met tekstinvoer/s2.java
+++ Formatieve opdrachten/Machine met tekstinvoer/s2.java
@@ -1,28 +1,19 @@
 package tobias.formatieveopdracht2a;
 
-import java.util.Scanner;
-
 public class s2 extends State {
 
     void enter() {
         System.out.println("Nu in de s2 state");
     }
 
-    void update() {
-        while (true) {
-            System.out.println("B");
-
-            Scanner s = new Scanner(System.in);
-            String input = s.next();
-
-            switch (input) {
-
-                case "B":
-                    current = s3;
-                    return;
-                default:
-                    System.out.println("\n*** Error, overgang bestaat niet");
-            }
+    String update(String input) {
+        switch (input) {
+            case "B":
+                current = s3;
+                return "s3";
+            default:
+                System.out.println("\n*** Error, overgang bestaat niet");
+                return "fout";
         }
     }
 }
