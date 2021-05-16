152fafc0883283b153ab51344e2e9303256ad8b0
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 22:55:57 +0200

    Add files via upload

---
+++ Formatieve opdrachten/Machine met tekstinvoer/State.java
@@ -0,0 +1,10 @@
+package tobias.formatieveopdracht2a;
+
+abstract class State {
+
+    static tobias.formatieveopdracht2a.State s0, s1, s2, s3, current;
+
+    void enter() { }
+    void update() { }
+
+}
