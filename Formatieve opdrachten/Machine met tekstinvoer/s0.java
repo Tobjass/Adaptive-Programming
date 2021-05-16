6b71a6dbeef994dd858d3049cf0d66ae324b4ab1
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 23:55:33 +0200

    Update s0.java
    Prints voor opties weggehaald
    Oneindige loop weggehaald
    update() returnt nu een string

--- Formatieve opdrachten/Machine met tekstinvoer/s0.java
+++ Formatieve opdrachten/Machine met tekstinvoer/s0.java
@@ -1,32 +1,22 @@
 package tobias.formatieveopdracht2a;
 
-import java.util.Scanner;
-
 public class s0 extends State {
 
     void enter() {
         System.out.println("Nu in de s0 state");
     }
 
-    void update() {
-        while (true) {
-            System.out.println("A");
-            System.out.println("B");
-
-            Scanner s = new Scanner(System.in);
-            String input = s.next();
-
-            switch (input) {
-
-                case "A":
-                    current = s2;
-                    return;
-                case "B":
-                    current = s1;
-                    return;
-                default:
-                    System.out.println("\n*** Error, overgang bestaat niet");
-            }
+    String update(String input) {
+        switch (input) {
+            case "A":
+                current = s2;
+                return "s2";
+            case "B":
+                current = s1;
+                return "s1";
+            default:
+                System.out.println("\n*** Error, overgang bestaat niet");
+                return "fout";
         }
     }
 }
