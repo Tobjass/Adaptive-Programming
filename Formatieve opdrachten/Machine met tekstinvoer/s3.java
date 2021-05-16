85af24294e0eed539fc9a54751444236a1fea1e1
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 23:08:06 +0200

    Update s3.java
    Code van s0 toegevoegd en aangepast

--- Formatieve opdrachten/Machine met tekstinvoer/s3.java
+++ Formatieve opdrachten/Machine met tekstinvoer/s3.java
@@ -1,6 +1,32 @@
 package tobias.formatieveopdracht2a;
 
+import java.util.Scanner;
+
 public class s3 extends State {
 
+    void enter() {
+        System.out.println("Nu in de s3 state");
+    }
 
+    void update() {
+        while (true) {
+            System.out.println("A");
+            System.out.println("B");
+
+            Scanner s = new Scanner(System.in);
+            String input = s.next();
+
+            switch (input) {
+
+                case "A":
+                    current = s3;
+                    return;
+                case "B":
+                    current = s0;
+                    return;
+                default:
+                    System.out.println("\n*** Error, overgang bestaat niet");
+            }
+        }
+    }
 }
