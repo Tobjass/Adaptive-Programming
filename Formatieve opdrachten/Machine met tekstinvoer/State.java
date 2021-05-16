4f92315f30c244c334df31be57cd478be9598ca2
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 22:56:48 +0200

    Update State.java

--- Formatieve opdrachten/Machine met tekstinvoer/State.java
+++ Formatieve opdrachten/Machine met tekstinvoer/State.java
@@ -2,7 +2,7 @@ package tobias.formatieveopdracht2a;
 
 abstract class State {
 
-    static tobias.formatieveopdracht2a.State s0, s1, s2, s3, current;
+    static State s0, s1, s2, s3, current;
 
     void enter() { }
     void update() { }
