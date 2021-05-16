455c90028e508e82bf5a7c1b255eb615888b3306
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 23:05:19 +0200

    Update s1.java
    Code van s0 toegevoegd en aangepast

--- Formatieve opdrachten/Machine met tekstinvoer/s1.java
+++ Formatieve opdrachten/Machine met tekstinvoer/s1.java
@@ -1,6 +1,32 @@
 package tobias.formatieveopdracht2a;
 
+import java.util.Scanner;
+
 public class s1 extends State {
 
-    
+    void enter() {
+        System.out.println("Nu in de s1 state");
+    }
+
+    void update() {
+        while (true) {
+            System.out.println("A");
+            System.out.println("B");
+
+            Scanner s = new Scanner(System.in);
+            String input = s.next();
+
+            switch (input) {
+
+                case "A":
+                    current = s1;
+                    return;
+                case "B":
+                    current = s2;
+                    return;
+                default:
+                    System.out.println("\n*** Error, overgang bestaat niet");
+            }
+        }
+    }
 }
