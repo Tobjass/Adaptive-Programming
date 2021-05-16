617d084d6b05527539db3575f6aa505faf809dbc
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-16 23:06:53 +0200

    Update s2.java
    Code van s0 toegevoegd en aangepast

--- Formatieve opdrachten/Machine met tekstinvoer/s2.java
+++ Formatieve opdrachten/Machine met tekstinvoer/s2.java
@@ -1,6 +1,28 @@
 package tobias.formatieveopdracht2a;
 
+import java.util.Scanner;
+
 public class s2 extends State {
 
+    void enter() {
+        System.out.println("Nu in de s2 state");
+    }
 
+    void update() {
+        while (true) {
+            System.out.println("B");
+
+            Scanner s = new Scanner(System.in);
+            String input = s.next();
+
+            switch (input) {
+
+                case "B":
+                    current = s3;
+                    return;
+                default:
+                    System.out.println("\n*** Error, overgang bestaat niet");
+            }
+        }
+    }
 }
