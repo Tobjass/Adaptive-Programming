7737a48e0b91d6cd59c02fb26c66a5b4e0b089e7
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-18 22:36:14 +0200

    Update s0.java
    enter() weggehaald
    update() aangepast
    toString() toegevoegd

--- Formatieve opdrachten/Machine met tekstinvoer/s0.java
+++ Formatieve opdrachten/Machine met tekstinvoer/s0.java
@@ -2,21 +2,23 @@ package tobias.formatieveopdracht2a;
 
 public class s0 extends State {
 
-    void enter() {
-        System.out.println("Nu in de s0 state");
-    }
-
-    String update(String input) {
+    void update(String input) {
         switch (input) {
             case "A":
                 current = s2;
-                return "s2";
+                State.setLocaties(s2);
+                return;
             case "B":
                 current = s1;
-                return "s1";
+                State.setLocaties(s1);
+                return;
             default:
-                System.out.println("\n*** Error, overgang bestaat niet");
-                return "fout";
+                System.out.println("*** Error, overgang (s0 -> " + input + ") bestaat niet");
         }
+    }
+
+    @Override
+    public String toString() {
+        return "s0";
     }
 }
