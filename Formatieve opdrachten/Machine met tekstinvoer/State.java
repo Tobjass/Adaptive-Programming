4c8c853e1446a37068010dd76f9bf600c9d41784
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-18 22:35:40 +0200

    Update State.java
    enter() weggehaald
    Locaties lijst toegevoegd
    Setter en getter voor locaties toegevoegd
    update() krijgt bij aanroepen nu een string mee

--- Formatieve opdrachten/Machine met tekstinvoer/State.java
+++ Formatieve opdrachten/Machine met tekstinvoer/State.java
@@ -1,10 +1,21 @@
 package tobias.formatieveopdracht2a;
 
-abstract class State {
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
 
     static State s0, s1, s2, s3, current;
 
-    void enter() { }
-    void update() { }
+    private static List<State> locaties = new ArrayList<>();
 
+    public static void setLocaties(State locatie) {
+        locaties.add(locatie);
+    }
+
+    public static List<State> getLocaties() {
+        return locaties;
+    }
+
+    void update(String input) { }
 }
