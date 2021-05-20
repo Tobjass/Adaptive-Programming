910b381e847a468b2342e4892af1427bb2ce54a7
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-20 16:29:06 +0200

    Update State.java
    addTransitie() toegevoegd

--- Summatieve opdrachten/2. Finite State Machine & Testing/State.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/State.java
@@ -4,18 +4,18 @@ import java.util.HashMap;
 
 public class State {
 
-    private HashMap<String, tobias.formatieveopdracht2a.State> transities = new HashMap<>();
+    private HashMap<String, State> transities = new HashMap<>();
     private String naam;
 
     public State(String naam) {
         this.naam = naam;
     }
 
-    public HashMap<String, tobias.formatieveopdracht2a.State> getTransities() {
+    public HashMap<String, State> getTransities() {
         return transities;
     }
 
-    public void setTransities(HashMap<String, tobias.formatieveopdracht2a.State> transities) {
+    public void setTransities(HashMap<String, State> transities) {
         this.transities = transities;
     }
 
@@ -25,5 +25,9 @@ public class State {
 
     public void setNaam(String naam) {
         this.naam = naam;
+    }
+
+    public void addTransitie(String optie, State state) {
+        this.transities.put(optie, state);
     }
 }
