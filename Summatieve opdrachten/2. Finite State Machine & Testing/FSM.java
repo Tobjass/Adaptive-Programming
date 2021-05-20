a90df9d773d16969ce9961a5493e01a4da0c6e4e
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-20 22:54:38 +0200

    Update FSM.java
    Variables states en locaties toegevoegd

--- Summatieve opdrachten/2. Finite State Machine & Testing/FSM.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/FSM.java
@@ -1,4 +1,10 @@
 package tobias.summatieveopdracht2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FSM {
+
+    private State[] states;
+    private List<String> locaties = new ArrayList<>();
 }
