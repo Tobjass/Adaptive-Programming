963e97de5ce3d87ad1d182d1a39e2ebe07e9e1c7
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-20 16:26:35 +0200

    Update Main.java
    State s0 t/m s9 aangemaakt

--- Summatieve opdrachten/2. Finite State Machine & Testing/Main.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/Main.java
@@ -1,4 +1,15 @@
 package tobias.summatieveopdracht2;
 
 public class Main {
+
+    State s0 = new State("s0");
+    State s1 = new State("s1");
+    State s2 = new State("s2");
+    State s3 = new State("s3");
+    State s4 = new State("s4");
+    State s5 = new State("s5");
+    State s6 = new State("s6");
+    State s7 = new State("s7");
+    State s8 = new State("s8");
+    State s9 = new State("s9");
 }
