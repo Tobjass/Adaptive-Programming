a5b3c5877dc826d11f043cc21fb682caffb77bf2
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-21 12:08:47 +0200

    Update MainTest.java
    nieuweTransitie() test toegevoegd

--- Summatieve opdrachten/2. Finite State Machine & Testing/MainTest.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/MainTest.java
@@ -11,4 +11,14 @@ public class MainTest {
 
         assertEquals("Error bij het aanmaken van nieuwe state", "s0", s0.getNaam());
     }
+
+    @Test
+    public void nieuweTransitie() {
+        State s0 = new State("s0");
+        State s3 = new State("s3");
+
+        s0.addTransitie("A", s3);
+
+        assertEquals("Error bij het aanmaken van nieuwe transitie", "State: s0 Transities: A -> s3, ", s0.toString());
+    }
 }
