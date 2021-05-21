9f2c4f4c5a84f3c9cf4611e7ac8b438be792f89b
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-21 13:31:55 +0200

    Update FSM.java
    Commentaar toegevoegd

--- Summatieve opdrachten/2. Finite State Machine & Testing/FSM.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/FSM.java
@@ -8,19 +8,28 @@ public class FSM {
     private State[] states;
     private List<String> locaties = new ArrayList<>();
 
+    // Constructor
     public FSM(State[] states) {
         this.states = states;
     }
 
+    // Runnen van de Finite State Machine
     public List<String> run(String[] inputs, State beginState) {
+        // Beginstate aan current toewijzen
         State current = beginState;
+        // Naam van beginstate aan de locaties toevoegen
         this.locaties.add(current.getNaam());
+        // For loop die elke letter uit inputs langsgaat
         for (String input : inputs) {
+            // Als de letter in de transities aanwezig is
             if (current.getTransitiesSet().contains(input)) {
+                // Current updaten
                 current = current.getTransities().get(input);
+                // Naam van nieuwe state aan de locaties toevoegen
                 this.locaties.add(current.getNaam());
             }
             else {
+                // Error printen en aangeven dat een input bij een bepaalde state niet bestaat
                 System.out.println("\nError: overgang '" + input + "' bestaat niet voor state " + current.getNaam());
             }
         }
