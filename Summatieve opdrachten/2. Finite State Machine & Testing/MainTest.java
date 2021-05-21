007169e639e16eeb1462c265e489d05bc7455d66
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-21 12:04:00 +0200

    Update MainTest.java
    nieuweState() test toegevoegd

--- Summatieve opdrachten/2. Finite State Machine & Testing/MainTest.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/MainTest.java
@@ -1,4 +1,14 @@
 package tobias.summatieveopdracht2;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class MainTest {
+
+    @Test
+    public void nieuweState() {
+        State s0 = new State("s0");
+
+        assertEquals("Error bij het aanmaken van nieuwe state", "s0", s0.getNaam());
+    }
 }
