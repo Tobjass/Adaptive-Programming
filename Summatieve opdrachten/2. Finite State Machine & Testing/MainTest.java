5d531a82fd6f96dc223f168adc57b20abfec9e36
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-21 12:19:54 +0200

    Update MainTest.java
    FSMrun() test aangemaakt

--- Summatieve opdrachten/2. Finite State Machine & Testing/MainTest.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/MainTest.java
@@ -3,6 +3,9 @@ package tobias.summatieveopdracht2;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainTest {
 
     @Test
@@ -20,5 +23,26 @@ public class MainTest {
         s0.addTransitie("A", s3);
 
         assertEquals("Error bij het aanmaken van nieuwe transitie", "State: s0 Transities: A -> s3, ", s0.toString());
+    }
+
+    @Test
+    public void FSMrun() {
+        State s0 = new State("s0");
+        State s1 = new State("s1");
+        State s2 = new State("s2");
+
+        s0.addTransitie("D", s1);
+        s1.addTransitie("A", s2);
+
+        State[] states = {s0, s1, s2};
+        FSM fsm = new FSM(states);
+
+        String[] inputs = {"D", "A"};
+        List<String> locaties = new ArrayList<>();
+        locaties.add("s0");
+        locaties.add("s1");
+        locaties.add("s2");
+        
+        assertEquals("Error bij het runnen van FSM", locaties, fsm.run(inputs, s0));
     }
 }
