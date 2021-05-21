73fe91bf42f4dbd160a351dc52bcd84d1df3d1ba
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-21 13:28:13 +0200

    Update State.java
    Commentaar toegevoegd

--- Summatieve opdrachten/2. Finite State Machine & Testing/State.java
+++ Summatieve opdrachten/2. Finite State Machine & Testing/State.java
@@ -7,27 +7,33 @@ public class State {
 
     private HashMap<String, State> transities = new HashMap<>();
     private String naam;
-
+    
+    // Constructor
     public State(String naam) {
         this.naam = naam;
     }
-
+    
+    // Ophalen van de transities 
     public HashMap<String, State> getTransities() {
         return transities;
     }
-
+    
+    // Ophalen van de transities in een set om te kunnen controleren om de input geldig is
     public Set<String> getTransitiesSet() {
         return transities.keySet();
     }
-
+    
+    // Ophalen van de state-naam
     public String getNaam() {
         return naam;
     }
-
+    
+    // Transitie aan de state toevoegen
     public void addTransitie(String optie, State state) {
         this.transities.put(optie, state);
     }
-
+    
+    // Transities van de state overzichtelijk returnen
     @Override
     public String toString() {
         String transitiesString = "";
