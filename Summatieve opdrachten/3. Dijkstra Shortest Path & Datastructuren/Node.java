2cce2014f61bac3579ecba2058bc4141a7982feb
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-28 22:34:01 +0200

    Update Node.java
    Constructor toegevoegd

--- Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Node.java
+++ Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Node.java
@@ -4,4 +4,9 @@ public class Node {
 
     public int node;
     public int afstandVanBeginpunt;
+
+    public Node(int node, int afstandVanBeginpunt) {
+        this.node = node;
+        this.afstandVanBeginpunt = afstandVanBeginpunt;
+    }
 }
