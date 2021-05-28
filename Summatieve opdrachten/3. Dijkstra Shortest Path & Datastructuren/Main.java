18eb4a8c11aee26f2749b77e5526a6672d987ace
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-28 23:13:19 +0200

    Update Main.java
    String lijst met treinstations toegevoegd
    ArrayList aanmaken voor data

--- Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Main.java
+++ Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Main.java
@@ -1,6 +1,13 @@
 package tobias.summatieveopdracht3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    
+    public static void main(String[] args) {
+        String[] treinstations = {"Utrecht Centraal", "Amsterdam Centraal", "Rotterdam Centraal", "Schiphol Airport", "Den Haag Centraal", "Leiden Centraal", "Eindhoven Centraal", "’s-Hertogenbosch"};
+
+        List<List<Node>> data = new ArrayList<>();
+    }
 }
