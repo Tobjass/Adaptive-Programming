5c1c69edd136396d79a126ff799be548fb9b784c
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-28 22:31:10 +0200

    Update Reis.java
    Constructor toegevoegd

--- Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Reis.java
+++ Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Reis.java
@@ -7,4 +7,10 @@ public class Reis {
     private int afstand[];
     private Set<Integer> visited;
     private int punten;
+
+    public Reis(int punten) {
+        this.punten = punten;
+        afstand = new int[punten];
+        visited = new HashSet<>();
+    }
 }
