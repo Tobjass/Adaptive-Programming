cfdddf9ea17a36b2e9e0a4a04ea80ce10a7f8a58
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-28 22:41:54 +0200

    Update Node.java
    Comparator toegevoegd (doet nu nog niks)

--- Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Node.java
+++ Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Node.java
@@ -1,6 +1,8 @@
 package tobias.summatieveopdracht3;
 
-public class Node {
+import java.util.Comparator;
+
+public class Node implements Comparator<Node> {
 
     public int node;
     public int afstandVanBeginpunt;
@@ -10,5 +12,10 @@ public class Node {
     public Node(int node, int afstandVanBeginpunt) {
         this.node = node;
         this.afstandVanBeginpunt = afstandVanBeginpunt;
+    }
+
+    @Override
+    public int compare(Node o1, Node o2) {
+        return 0;
     }
 }
