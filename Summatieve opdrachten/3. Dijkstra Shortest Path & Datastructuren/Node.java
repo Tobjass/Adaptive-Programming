e9206e8f27f25613ab79cb7d33c32d67bdfcc9ba
Tobjass <74547189+Tobjass@users.noreply.github.com>
2021-05-30 15:46:49 +0200

    Update Node.java
    Commentaar toegevoegd

--- Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Node.java
+++ Summatieve opdrachten/3. Dijkstra Shortest Path & Datastructuren/Node.java
@@ -7,13 +7,16 @@ public class Node implements Comparator<Node> {
     public int node;
     public double afstandVanBeginpunt;
 
+    // Lege constructor
     public Node() { }
 
+    // Constructor
     public Node(int node, double afstandVanBeginpunt) {
         this.node = node;
         this.afstandVanBeginpunt = afstandVanBeginpunt;
     }
 
+    // Compare functie
     @Override
     public int compare(Node o1, Node o2) {
         if (o1.afstandVanBeginpunt < o2.afstandVanBeginpunt)
